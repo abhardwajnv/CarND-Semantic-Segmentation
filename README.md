f268b3529be540acf09907e3646c146833a4d5c5
Amit Bhardwaj <abhardwaj@nvidia.com>
2017-12-26 22:07:22 +0530

    Final Commit - Project2 Submission

--- README.md
+++ README.md
@@ -34,3 +34,66 @@ python main.py
  
  ## How to write a README
 A well written README file can enhance your project and portfolio.  Develop your abilities to create professional README files by completing [this free course](https://www.udacity.com/course/writing-readmes--ud777).
+
+Output:
+------------------------------------------------------------------------------------------
+Epoch: 1 / 58  Loss: 5.612
+Epoch: 2 / 58  Loss: 2.011
+Epoch: 3 / 58  Loss: 1.172
+Epoch: 4 / 58  Loss: 0.946
+Epoch: 5 / 58  Loss: 0.856
+Epoch: 6 / 58  Loss: 0.782
+Epoch: 7 / 58  Loss: 0.762
+Epoch: 8 / 58  Loss: 0.741
+Epoch: 9 / 58  Loss: 0.699
+Epoch: 10 / 58  Loss: 0.703
+Epoch: 11 / 58  Loss: 0.691
+Epoch: 12 / 58  Loss: 0.701
+Epoch: 13 / 58  Loss: 0.689
+Epoch: 14 / 58  Loss: 0.672
+Epoch: 15 / 58  Loss: 0.675
+Epoch: 16 / 58  Loss: 0.667
+Epoch: 17 / 58  Loss: 0.662
+Epoch: 18 / 58  Loss: 0.669
+Epoch: 19 / 58  Loss: 0.656
+Epoch: 20 / 58  Loss: 0.659
+Epoch: 21 / 58  Loss: 0.637
+Epoch: 22 / 58  Loss: 0.611
+Epoch: 23 / 58  Loss: 0.619
+Epoch: 24 / 58  Loss: 0.592
+Epoch: 25 / 58  Loss: 0.601
+Epoch: 26 / 58  Loss: 0.602
+Epoch: 27 / 58  Loss: 0.574
+Epoch: 28 / 58  Loss: 0.555
+Epoch: 29 / 58  Loss: 0.536
+Epoch: 30 / 58  Loss: 0.522
+Epoch: 31 / 58  Loss: 0.525
+Epoch: 32 / 58  Loss: 0.478
+Epoch: 33 / 58  Loss: 0.479
+Epoch: 34 / 58  Loss: 0.390
+Epoch: 35 / 58  Loss: 0.414
+Epoch: 36 / 58  Loss: 0.399
+Epoch: 37 / 58  Loss: 0.362
+Epoch: 38 / 58  Loss: 0.371
+Epoch: 39 / 58  Loss: 0.297
+Epoch: 40 / 58  Loss: 0.258
+Epoch: 41 / 58  Loss: 0.261
+Epoch: 42 / 58  Loss: 0.188
+Epoch: 43 / 58  Loss: 0.270
+Epoch: 44 / 58  Loss: 0.141
+Epoch: 45 / 58  Loss: 0.138
+Epoch: 46 / 58  Loss: 0.156
+Epoch: 47 / 58  Loss: 0.250
+Epoch: 48 / 58  Loss: 0.330
+Epoch: 49 / 58  Loss: 0.161
+Epoch: 50 / 58  Loss: 0.144
+Epoch: 51 / 58  Loss: 0.206
+Epoch: 52 / 58  Loss: 0.079
+Epoch: 53 / 58  Loss: 0.150
+Epoch: 54 / 58  Loss: 0.161
+Epoch: 55 / 58  Loss: 0.164
+Epoch: 56 / 58  Loss: 0.127
+Epoch: 57 / 58  Loss: 0.092
+Epoch: 58 / 58  Loss: 0.163
+Training Finished. Saving test images to: ./runs/1514304922.065686
+------------------------------------------------------------------------------------------
--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def load_vgg(sess, vgg_path):
     Load Pretrained VGG Model into TensorFlow.
     :param sess: TensorFlow Session
     :param vgg_path: Path to vgg folder, containing "variables/" and "saved_model.pb"
-    :return: Tuple of Tensors from VGG model (image_input, keep_prob, layer3_out, layer4_out, layer7_out)
+    :return: Tuple of Tensors from VGG model (img_inp, keep_prob, layer3_out, layer4_out, layer7_out)
     """
     # TODO: Implement function
     #   Use tf.saved_model.loader.load to load the model and weights
@@ -32,8 +32,16 @@ def load_vgg(sess, vgg_path):
     vgg_layer3_out_tensor_name = 'layer3_out:0'
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
+
+    tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
+    graph = tf.get_default_graph()
+    img_inp = graph.get_tensor_by_name(vgg_input_tensor_name)
+    keep_prob = graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
+    layer3_out = graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
+    layer4_out = graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
+    layer7_out = graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
     
-    return None, None, None, None, None
+    return img_inp, keep_prob, layer3_out, layer4_out, layer7_out
 tests.test_load_vgg(load_vgg, tf)
 
 
@@ -47,7 +55,18 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :return: The Tensor for the last layer of output
     """
     # TODO: Implement function
-    return None
+    layer7_conv_1x1 = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, strides=(1,1), padding='same', kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    out = tf.layers.conv2d_transpose(layer7_conv_1x1, num_classes, 4, strides=(2,2), padding='same', kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    
+    layer4_conv_1x1 = tf.layers.conv2d(vgg_layer4_out, num_classes, 1, strides=(1,1), padding='same', kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    out = tf.add(out, layer4_conv_1x1)
+    out = tf.layers.conv2d_transpose(out, num_classes, 4, strides=(2,2), padding='same', kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+
+    layer3_conv_1x1 = tf.layers.conv2d(vgg_layer3_out, num_classes, 1, strides=(1,1), padding='same', kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+    out = tf.add(out, layer3_conv_1x1)
+    out = tf.layers.conv2d_transpose(out, num_classes, 16, strides=(8,8), padding='same', kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+
+    return out
 tests.test_layers(layers)
 
 
@@ -61,7 +80,13 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
     # TODO: Implement function
-    return None, None, None
+    logits = tf.reshape(nn_last_layer, (-1, num_classes))
+    correct_label = tf.reshape(correct_label, (-1, num_classes))
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=correct_label))
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+    train_op = optimizer.minimize(cross_entropy_loss)
+
+    return logits, train_op, cross_entropy_loss
 tests.test_optimize(optimize)
 
 
@@ -81,7 +106,13 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     :param learning_rate: TF Placeholder for learning rate
     """
     # TODO: Implement function
-    pass
+    lrate = 0.0001
+    prob = 0.5    
+    for epoch in range(1, epochs+1):
+        for (images, labels) in get_batches_fn(batch_size):
+            # Training 
+            loss = sess.run([train_op, cross_entropy_loss], feed_dict={input_image: images, correct_label: labels, keep_prob: prob, learning_rate: lrate})
+        print("Epoch: {}".format(epoch), "/ {}".format(epochs), " Loss: {:.3f}".format(loss[1]))
 tests.test_train_nn(train_nn)
 
 
@@ -90,6 +121,10 @@ def run():
     image_shape = (160, 576)
     data_dir = './data'
     runs_dir = './runs'
+    epochs = 58
+    batch_size = 16
+    correct_label = tf.placeholder(tf.float32, (None, None, None, num_classes), name='correct_label')
+    learning_rate = tf.placeholder(tf.float32, name='learning_rate')
     tests.test_for_kitti_dataset(data_dir)
 
     # Download pretrained vgg model
@@ -109,11 +144,17 @@ def run():
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
 
         # TODO: Build NN using load_vgg, layers, and optimize function
+        img_inp, keep_prob, layer3_out, layer4_out, layer7_out = load_vgg(sess, vgg_path)
+        layer_out = layers(layer3_out, layer4_out, layer7_out, num_classes)
+        logits, train_op, cross_entropy_loss = optimize(layer_out, correct_label, learning_rate, num_classes)
 
         # TODO: Train NN using the train_nn function
+        sess.run(tf.global_variables_initializer())
+        train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, img_inp, correct_label, keep_prob, learning_rate)
 
         # TODO: Save inference data using helper.save_inference_samples
         #  helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
+        helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, img_inp)
 
         # OPTIONAL: Apply the trained model to a video
 
